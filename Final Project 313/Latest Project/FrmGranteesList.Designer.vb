0d96bc6829f6140bbc53956ea414bf4f7b23b547
Yandaveeee <ianpunayo09@gmail.com>
2025-11-06 23:44:55 +0800

    Add files via upload

---
+++ Final Project 313/Latest Project/FrmGranteesList.Designer.vb
@@ -0,0 +1,126 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class FrmGranteesList
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer.
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.PanelTop = New System.Windows.Forms.Panel()
+        Me.btnRefresh = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.btnEdit = New System.Windows.Forms.Button()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.dgvGrantees = New System.Windows.Forms.DataGridView()
+        Me.PanelTop.SuspendLayout()
+        CType(Me.dgvGrantees, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PanelTop
+        '
+        Me.PanelTop.Controls.Add(Me.btnRefresh)
+        Me.PanelTop.Controls.Add(Me.btnDelete)
+        Me.PanelTop.Controls.Add(Me.btnEdit)
+        Me.PanelTop.Controls.Add(Me.btnAdd)
+        Me.PanelTop.Dock = System.Windows.Forms.DockStyle.Top
+        Me.PanelTop.Location = New System.Drawing.Point(0, 0)
+        Me.PanelTop.Name = "PanelTop"
+        Me.PanelTop.Size = New System.Drawing.Size(800, 60)
+        Me.PanelTop.TabIndex = 0
+        '
+        'btnRefresh
+        '
+        Me.btnRefresh.Location = New System.Drawing.Point(400, 15)
+        Me.btnRefresh.Name = "btnRefresh"
+        Me.btnRefresh.Size = New System.Drawing.Size(100, 30)
+        Me.btnRefresh.TabIndex = 3
+        Me.btnRefresh.Text = "Refresh"
+        Me.btnRefresh.UseVisualStyleBackColor = True
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Location = New System.Drawing.Point(260, 15)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(130, 30)
+        Me.btnDelete.TabIndex = 2
+        Me.btnDelete.Text = "Delete Selected"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'btnEdit
+        '
+        Me.btnEdit.Location = New System.Drawing.Point(130, 15)
+        Me.btnEdit.Name = "btnEdit"
+        Me.btnEdit.Size = New System.Drawing.Size(120, 30)
+        Me.btnEdit.TabIndex = 1
+        Me.btnEdit.Text = "Edit Selected"
+        Me.btnEdit.UseVisualStyleBackColor = True
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(20, 15)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(100, 30)
+        Me.btnAdd.TabIndex = 0
+        Me.btnAdd.Text = "Add New"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'dgvGrantees
+        '
+        Me.dgvGrantees.AllowUserToAddRows = False
+        Me.dgvGrantees.AllowUserToDeleteRows = False
+        Me.dgvGrantees.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.dgvGrantees.BackgroundColor = System.Drawing.Color.White
+        Me.dgvGrantees.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvGrantees.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.dgvGrantees.Location = New System.Drawing.Point(0, 60)
+        Me.dgvGrantees.MultiSelect = False
+        Me.dgvGrantees.Name = "dgvGrantees"
+        Me.dgvGrantees.ReadOnly = True
+        Me.dgvGrantees.RowHeadersVisible = False
+        Me.dgvGrantees.RowHeadersWidth = 62
+        Me.dgvGrantees.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.dgvGrantees.Size = New System.Drawing.Size(800, 390)
+        Me.dgvGrantees.TabIndex = 1
+        '
+        'FrmGranteesList
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.dgvGrantees)
+        Me.Controls.Add(Me.PanelTop)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "FrmGranteesList"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Grantees"
+        Me.PanelTop.ResumeLayout(False)
+        CType(Me.dgvGrantees, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PanelTop As Panel
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnEdit As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents btnRefresh As Button
+    Friend WithEvents dgvGrantees As DataGridView
+End Class
